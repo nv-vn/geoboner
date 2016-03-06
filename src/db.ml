7d0f7154de8cb8bc3c574be755614ee6901cfbbe
nv-vn <nv@cock.li>
2016-03-06 02:42:56 -0500

    Swear imma kill myself if i dont find out why this is broken

--- src/db.ml
+++ src/db.ml
@@ -5,10 +5,12 @@ open Gensqlite_tools
 let db = Sqlite3.db_open "data/users.db"
 
 module Raw = struct
-  let (_, get_user) = [%gensqlite db "SELECT @s{firstname}, @s{lastname}, @s{username}, @s{boners}\
+  let (_, get_user) = [%gensqlite db "SELECT @s{firstname}, @s{lastname}, @s{boners}\
                                       FROM users WHERE username = %s{username}"]
-  let (_, put_user) = [%gensqlite db "INSERT OR REPLACE INTO users (firstname, lastname, username, boners)\
-                                      VALUES (%s{firstname}, %s{lastname}, %s{username}, %s{boners})"]
+  let (_, new_user) = [%gensqlite db "INSERT INTO users (firstname, lastname, username, boners) VALUES\
+                                      (%s{firstname}, %s{lastname}, %s{username}, %s{boners})"]
+  let (_, put_user) = [%gensqlite db "UPDATE users SET firstname=%s{firstname}, lastname=%s{lastname},\
+                                      username=%s{username}, boners=%s{boners} WHERE username=%s{username}"]
 end
 
 let get_user username =
@@ -18,10 +20,17 @@ let get_user username =
     | _ -> [] in
   match user with
   | [] -> create_user ~firstname:"error" ~lastname:"error" ~username
-  | (firstname, lastname, _, boners)::_ ->
+  | (firstname, lastname, boners)::_ ->
     {(create_user ~firstname ~lastname ~username) with
      boners = Yojson.Safe.from_string boners
               |> elems |> List.map boner_of_json}
 
+let new_user {firstname; lastname; username; boners} =
+  let boners' = List.map json_of_boner boners in
+  let boners = `List boners' |> Yojson.Safe.to_string in
+  Raw.new_user ~firstname ~lastname ~username ~boners ()
+
 let put_user {firstname; lastname; username; boners} =
-  ()
+  let boners' = List.map json_of_boner boners in
+  let boners = `List boners' |> Yojson.Safe.to_string in
+  Raw.put_user ~firstname ~lastname ~username ~boners ()
--- src/main.ml
+++ src/main.ml
@@ -9,6 +9,28 @@ let user_info = get "/user/:username" begin fun req ->
     `String json |> respond'
   end
 
+let new_user = post "/new/" begin fun req ->
+    App.urlencoded_pairs_of_body req >>= fun params ->
+    let post_param name = List.assoc name params |> List.hd in
+    let firstname = post_param "firstname"
+    and lastname  = post_param "lastname"
+    and username  = post_param "username" in
+    create_user ~firstname ~lastname ~username |> Db.new_user;
+    `String [%blob "../static/user.html"] |> respond'
+  end
+
+let log_boner = post "/log/" begin fun req ->
+    App.urlencoded_pairs_of_body req >>= fun params ->
+    let post_param name = List.assoc name params |> List.hd in
+    let username  = post_param "username"
+    and longitude = float_of_string @@ post_param "longitude"
+    and latitude  = float_of_string @@ post_param "latitude" in
+    let user = Db.get_user username in
+    let boner = create_boner ~longitude ~latitude in
+    add_boner boner user |> Db.put_user;
+    `String [%blob "../static/user.html"] |> respond'
+  end
+
 let index = get "/" begin fun req ->
     `String [%blob "../static/index.html"] |> respond'
   end
@@ -17,5 +39,7 @@ let app =
   App.empty
   |> index
   |> user_info
+  |> new_user
+  |> log_boner
 
-let () = app |> App.run_command
+let () = App.run_command app
